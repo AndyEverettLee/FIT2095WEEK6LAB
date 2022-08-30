242f60ce87c64111caf4285b7215e1cc39cfffc4
vellichdotnet <98381786+VellichA@users.noreply.github.com>
2022-08-30 14:25:26 +1000

    first commit

---
+++ Workshopquiz.js
@@ -0,0 +1,19 @@
+let express = require("express");
+let app = express();
+let path = require("path");
+let morgan = require("morgan");
+app.use(morgan('tiny'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html'); 
+
+app.use("/css",express.static(path-join(__dirname,"node_modules/bootstrap/dist/css")))
+app.use("/js",express.static(path-join(__dirname,"node_modules/bootstrap/dist/js")))
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const url = 'mongodb://localhost:27017/';
+let db; 
+
+
+app.listen(8080);
